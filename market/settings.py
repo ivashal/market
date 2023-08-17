f58aea66f1ab593306ed3f4ef122fbc47cbff4eb
z1l <shalaev_ivan@mail.ru>
2023-08-17 19:32:02 +0300

    Create new app

--- market/settings.py
+++ market/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
+    'cart',
+    'users',
+    'orders',
+    'products',
 ]
 
 MIDDLEWARE = [
---
+++ orders/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+
+app_name = 'orders'
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+]
