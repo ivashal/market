fa52edcb32af5190a7c4efd228fd6cd7ee44ad64
Ivan_Shalaev <shalaev_ivan@mail.ru>
2023-08-24 22:22:34 +0300

    Created HTML- files, and created forms in products

--- market/urls.py
+++ market/urls.py
@@ -26,7 +26,7 @@ from products.views import root_index
 urlpatterns = [
     path('', root_index, name='root_index'),
     path('admin/', admin.site.urls),
-    path('shop/', include(urls_products)),
+    path('products/', include(urls_products)),
     path('users/', include(urls_users)),
     path('cart/', include(urls_cart)),
     path('orders/', include(urls_orders)),
--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import index
+from .views import index, CategoryCreateView, CategoryListView
 
 
 app_name = 'products'
 urlpatterns = [
     path('', index, name='index'),
+    path('category-form/', CategoryCreateView.as_view(), name='category-form'),
+    path('category-list/', CategoryListView.as_view(), name='category-list')
     # path('admin/', admin.site.urls),
 ]
